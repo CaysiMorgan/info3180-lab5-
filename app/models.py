aac6714313bc9523f7008d47ce0377059f1fdca3
caysi morgan <caysi.s.morgan@gmail.com>
2023-04-11 21:52:10 -0500

    files added

--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+from datetime import datetime
+import pytz
+
+class Movies(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    description = db.Column(db.Text)
+    poster = db.Column(db.String(80))
+    created_at = db.Column(db.DateTime)
+
+    def __init__(self, title, description, poster):
+        self.title = title
+        self.description = description
+        self.poster = poster
+        time = datetime.now(pytz.timezone('US/Eastern'))
+        self.created_at = time
--- app/views.py
+++ app/views.py
@@ -4,10 +4,14 @@ Jinja2 Documentation:    https://jinja.palletsprojects.com/
 Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file creates your application.
 """
-
+from . import db
 from app import app
-from flask import render_template, request, jsonify, send_file
+from flask import render_template, request, jsonify, send_file, make_response, send_from_directory
+from app.models import Movies
+from app.forms import MovieForm
 import os
+from flask_wtf.csrf import generate_csrf
+from werkzeug.utils import secure_filename
 
 
 ###
@@ -18,7 +22,67 @@ import os
 def index():
     return jsonify(message="This is the beginning of our API")
 
+@app.route('/api/v1/movies', methods=['POST'])
+def movies():
+    form =MovieForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        desc = form.description.data
+        poster = form.poster.data
+        pname = secure_filename(poster.filename)
+        response=' '
+        response = jsonify({"message": "Movie added", 
+                    "title": title,
+                    "poster": pname, 
+                    "desc": desc})
+            
+        new_movie = Movies(title, desc, pname)
+        poster.save(os.path.join(app.config['UPLOAD_FOLDER'], pname))
+    
+        # adding to database
+        db.session.add(new_movie)
+        db.session.commit()
+        
+        return response
+        
+    else:
+        return make_response({'errors': form_errors(form)},400)
+    
 
+
+
+
+   
+@app.route('/api/v1/movies', methods = ['GET'])
+def get_movies():
+    movies = Movies.query.all()
+    
+    response = {'movies': []}
+    
+    
+    for movie in movies:
+        
+        movie_info = dict()
+        
+        movie_info['id'] = movie.id
+        movie_info['title'] = movie.title
+        movie_info['poster'] = f'/api/v1/posters/{movie.poster}'
+        movie_info['description'] = movie.description
+        
+        response['movies'] += [movie_info]
+        
+    return jsonify(response)
+
+            
+@app.route('/api/v1/posters/<filename>')
+def get_poster(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
+ 
+            
+@app.route('/api/v1/csrf-token', methods=['GET']) 
+def get_csrf():     
+    return jsonify({'csrf_token': generate_csrf()})            
+            
 ###
 # The functions below should be applicable to all Flask apps.
 ###
